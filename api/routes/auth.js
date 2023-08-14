43543f8efd31daab124b2bbf1fc54b75f7cbd8fc
Archer-Wangg <a519512409@gmail.com>
2023-08-14 17:44:00 +0800

    null

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,12 +1,12 @@
 import express from "express";
 import { login, logout, register } from "../controllers/auth.js";
-
+import cors from 'cors';
 
 const router = express.Router();
 
 router.get('/login', login);
 
-router.post('/register', register)
+router.post('/register',  register)
 router.post('/login', login)
 router.post('/logout', logout)
 
--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,8 +1,14 @@
 import express from "express";
-import { addPost } from "../controllers/post.js";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.js";
 
 const router = express.Router();
 
-router.get('/posts', addPost)
+router.get('/posts', getPosts)
+router.get('/posts/:id', getPost)
+router.post("/", addPost)
+router.delete('/:id', deletePost) 
+router.put('/', updatePost)
+
+
 
 export default router;
